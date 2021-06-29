e8b4aa5fb821dd0a0857a3204a61aa838aa108e2
Philip Mann <mannphilip7@gmail.com>
2021-06-29 18:41:04 -0500

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './components/form'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>My Form</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
