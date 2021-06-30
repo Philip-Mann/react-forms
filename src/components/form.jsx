441c1474ec32d127cfd8ecf998ff23ae1a6d5895
Philip Mann <mannphilip7@gmail.com>
2021-06-29 19:09:59 -0500

    updated some syntax

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -7,8 +7,7 @@ export default function Form() {
     const [contact, setContact] = useState({});
 
     const handleChange = e => {
-        const value = e.target.value;
-        const id = e.target.id;
+        const {id, value} = e.target
         const updatedContact = Object.assign({}, contact, {
             ...contact,     // ... splits things up into key: value pairs
             [id]: value     // adds to the contact object
@@ -26,46 +25,46 @@ export default function Form() {
 
                 <li>
                     <label htmlFor="lastName">Last Name</label>
-                    <input type="text" onChange={handleChange} value={contact.lastName} id="lastName" />
+                    <input type="text" onChange={handleChange} value={contact.lastName || ''} id="lastName" />
                 </li>
 
                 <li>  
                     <label htmlFor="street1">Street 1</label>
-                    <input type="text" onChange={handleChange} value={contact.street1} id="street1" />
+                    <input type="text" onChange={handleChange} value={contact.street1 || ''} id="street1" />
                 </li>
 
                 <li>
                     <label htmlFor="street2">Street 2</label>
-                    <input type="text" onChange={handleChange} value={contact.street2} id="street2" />
+                    <input type="text" onChange={handleChange} value={contact.street2 || ''} id="street2" />
                 </li>
 
                 <li>
                     <label htmlFor="city">City</label>
-                    <input type="text" onChange={handleChange} value={contact.city} id="city" />
+                    <input type="text" onChange={handleChange} value={contact.city || ''} id="city" />
                 </li>
 
                 <li>
                     <label htmlFor="state">State</label>
-                    <input type="text" onChange={handleChange} value={contact.state} id="state" />
+                    <input type="text" onChange={handleChange} value={contact.state || ''} id="state" />
                 </li>
 
                 <li>
-                    <label htmlFor="zip">Zip Code</label>
-                    <input type="text" onChange={handleChange} value={contact.zip} id="zip" />
+                    <label htmlFor="zipCode">Zip Code</label>
+                    <input type="text" onChange={handleChange} value={contact.zipCode || ''} id="zipCode" />
                 </li>
 
                 <li>
-                    <label htmlFor="tel">Telephone</label>
-                    <input type="text" onChange={handleChange} value={contact.tel} id="tel" />
+                    <label htmlFor="telephone">Telephone</label>
+                    <input type="text" onChange={handleChange} value={contact.telephone || ''} id="telephone" />
                 </li>
 
                 <li>
                     <label htmlFor="email">Email</label>
-                    <input type="text" onChange={handleChange} value={contact.email} id="email" />
+                    <input type="text" onChange={handleChange} value={contact.email || ''} id="email" />
                 </li>
             </ul>
             <button type="submit">Submit</button>
-        <p>{contact.firstName}</p>
+            <p>{JSON.stringify(contact)}</p>
         </form>
     );
 }
